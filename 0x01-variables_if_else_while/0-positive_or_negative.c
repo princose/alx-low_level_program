206976404a71809b4ef5bb803644abcde2fa5531
Your Name <you@example.com>
2022-12-09 12:03:21 -0800

    sign checker

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - prints if the number is positive,negative or 0
  * Return: Always 0
@@ -8,8 +9,10 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
